89168ebe833310eec40b3b6cff2c5a05e1bed88d
albacregalon <alba.cregalon@gmail.com>
2026-05-27 18:33:03 +0200

    Prueba 4

--- src/Article.java
+++ src/Article.java
@@ -1,11 +1,11 @@
 class Article {
     public String nombre;
     public int diesPerVendre;
-    public int qualidad;
+    public int calidad;
 
     public Article(String nom, int diesPerVendre, int qualitat) {
         this.nombre = nombre;
         this.diesPerVendre = diesPerVendre;
-        this.qualidad = qualidad;
+        this.calidad = calidad;
     }
 }
--- src/Magatzem.java
+++ src/Magatzem.java
@@ -9,25 +9,25 @@ class Magatzem {
         for (int i = 0; i < articles.length; i++) {
             if (!articles[i].nombre.equals("Formatge Gidurat")
                     && !articles[i].nombre.equals("Entrades per al Concert del Trobador")) {
-                if (articles[i].qualidad > 0) {
+                if (articles[i].calidad > 0) {
                     if (!articles[i].nombre.equals("Martell de Thor (Llegendari)")) {
-                        articles[i].qualidad = articles[i].qualidad - 1;
+                        articles[i].calidad = articles[i].calidad - 1;
                     }
                 }
             } else {
-                if (articles[i].qualidad < 50) {
-                    articles[i].qualidad = articles[i].qualidad + 1;
+                if (articles[i].calidad < 50) {
+                    articles[i].calidad = articles[i].calidad + 1;
 
                     if (articles[i].nombre.equals("Entrades per al Concert del Trobador")) {
                         if (articles[i].diesPerVendre < 11) {
-                            if (articles[i].qualidad < 50) {
-                                articles[i].qualidad = articles[i].qualidad + 1;
+                            if (articles[i].calidad < 50) {
+                                articles[i].calidad = articles[i].calidad + 1;
                             }
                         }
 
                         if (articles[i].diesPerVendre < 6) {
-                            if (articles[i].qualidad < 50) {
-                                articles[i].qualidad = articles[i].qualidad + 1;
+                            if (articles[i].calidad < 50) {
+                                articles[i].calidad = articles[i].calidad + 1;
                             }
                         }
                     }
@@ -41,17 +41,17 @@ class Magatzem {
             if (articles[i].diesPerVendre < 0) {
                 if (!articles[i].nombre.equals("Formatge Gidurat")) {
                     if (!articles[i].nombre.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].qualidad > 0) {
+                        if (articles[i].calidad > 0) {
                             if (!articles[i].nombre.equals("Martell de Thor (Llegendari)")) {
-                                articles[i].qualidad = articles[i].qualidad - 1;
+                                articles[i].calidad = articles[i].calidad - 1;
                             }
                         }
                     } else {
-                        articles[i].qualidad = articles[i].qualidad - articles[i].qualidad;
+                        articles[i].calidad = articles[i].calidad - articles[i].calidad;
                     }
                 } else {
-                    if (articles[i].qualidad < 50) {
-                        articles[i].qualidad = articles[i].qualidad + 1;
+                    if (articles[i].calidad < 50) {
+                        articles[i].calidad = articles[i].calidad + 1;
                     }
                 }
             }
